45307dafc9afb8ed21c7b950d70c9d582e37bbf3
Alena Belousova <alain-bels@yandex.ru>
2019-03-24 23:37:23 +0300

    add code for Test 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -16,4 +16,15 @@ public class MainClassTest {
         int result = number.getClassNumber();
         Assert.assertTrue("getClassNumber is lower then 45", result > 45);
     }
+
+    @Test
+    public void testGetClassString() {
+        MainClass main = new MainClass();
+        String result = main.getClassString();
+        Assert.assertTrue("getClassString not contains \"Hello\" or \"hello\"", checkStringToHello(result));
+    }
+
+    private Boolean checkStringToHello(String result) {
+        return result.contains("Hello") || result.contains("hello");
+    }
 }
