d8db7030ba910c4ded594db7689dd9ae5c2ce261
Alena Belousova <alain-bels@yandex.ru>
2019-03-24 22:26:46 +0300

    add code for Test 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,9 +4,16 @@ import org.junit.Test;
 
 public class MainClassTest {
     @Test
-    public void testGetLocalNumber(){
+    public void testGetLocalNumber() {
         MainClass number = new MainClass();
         int result = number.getLocalNumber();
         Assert.assertTrue("getLocalNumber is not 14", result == 14);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        MainClass number = new MainClass();
+        int result = number.getClassNumber();
+        Assert.assertTrue("getClassNumber is lower then 45", result > 45);
     }
 }
